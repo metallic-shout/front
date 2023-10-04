c4f5b048508d73d80a50d8931b769f3ae17ff64a
NULL-header <poaoq.poaoq@gmail.com>
2023-10-05 07:09:15 +0900

    fix double render

--- components/random-atom.tsx
+++ components/random-atom.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { useMount } from "react-use";
 import { makeShout } from "@/src/make-shout";
 import type { Atom } from "@/src/make-shout";
 import { ClipboardExec } from "@/components/clipboard";
@@ -9,11 +10,12 @@ const atomObj = process.env.ATOM_OBJ as unknown as Record<string, Atom>;
 const atoms: Atom[] = Object.values(atomObj);
 
 const useShout = () => {
-  const shout = useMemo(() => {
+  const [shout, setShout] = useState(null as string | null);
+  useMount(() => {
     const indexRandom = Math.round(Math.random() * atoms.length);
     const currentAtom = atoms[indexRandom];
-    return makeShout(currentAtom);
-  }, []);
+    setShout(makeShout(currentAtom));
+  });
   return shout;
 };
 
@@ -21,14 +23,20 @@ export const RandomAtom: React.FC = () => {
   const shout = useShout();
   return (
     <div className="w-full h-1/2 flex">
-      <div className="w-full h-full flex items-center">
-        <textarea
-          readOnly
-          value={shout}
-          className="bg-inherit w-3/4 ml-auto h-1/2 resize-none focus:outline-none"
-        />
-      </div>
-      <ClipboardExec target={shout} />
+      {shout == null ? (
+        ""
+      ) : (
+        <>
+          <div className="w-full h-full flex items-center">
+            <textarea
+              readOnly
+              value={shout}
+              className="bg-inherit w-3/4 ml-auto h-1/2 resize-none focus:outline-none"
+            />
+          </div>
+          <ClipboardExec target={shout} />
+        </>
+      )}
     </div>
   );
 };
