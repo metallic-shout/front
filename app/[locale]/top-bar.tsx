9d11ce8b0fb714f0372eb5b7ebe8bffd3e723b7d
NULL-header <poaoq.poaoq@gmail.com>
2024-03-02 17:23:53 +0900

    add locale switcher to top-bar

--- app/[locale]/top-bar.tsx
+++ app/[locale]/top-bar.tsx
@@ -1,7 +1,45 @@
-export const TopBar = () => {
+"use client";
+
+import { useMemo, useCallback, ReactEventHandler } from "react";
+import { useChangeLocale } from "@/components/locales/client";
+
+const locales = { ja: "JP", en: "EN" } as Record<string, string>;
+
+interface Props {
+  currentLocale: string;
+}
+
+export const TopBar = ({ currentLocale }: Props) => {
+  const branches = useMemo(
+    () => Object.entries(locales).filter(([e]) => e !== currentLocale),
+    [currentLocale]
+  );
+  const changeLocale = useChangeLocale();
+  const onSelect: ReactEventHandler<HTMLSelectElement> = useCallback(
+    (e) => {
+      changeLocale((e.target as any).value);
+    },
+    [changeLocale]
+  );
   return (
-    <div className="fixed items-start justify-start w-screen h-fit py-3 bg-panel pl-10">
-      <h1 className="bold text-3xl">Metallic shout</h1>
+    <div className="fixed justify-start w-screen h-fit py-3 bg-panel pl-10">
+      <h1 className="bold text-3xl whitespace-nowrap">Metallic shout</h1>
+      <div className="grow justify-end mr-5">
+        <select
+          className="bg-panel"
+          defaultValue={currentLocale}
+          onChange={onSelect}
+        >
+          <option value={currentLocale}>{locales[currentLocale]}</option>
+          {branches.map(([e, label]) => {
+            return (
+              <option key={e} value={e}>
+                {label}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     </div>
   );
 };
